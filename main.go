039a31e40c076d602d24a6b6571cddfdce114745
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-03-15 20:17:43 +0900

    init auth

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	firebase "firebase.google.com/go"
+
+	"google.golang.org/api/option"
+)
+
+func initAuth() (app *firebase.App, err error) {
+	opt := option.WithCredentialsFile("path/to/serviceAccountKey.json")
+	app, err = firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		return nil, fmt.Errorf("error initializing app: %v", err)
+	}
+	return app, nil
+}
 
 func main() {
+
 	fmt.Print("hello")
 }
