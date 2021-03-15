be3dc53ad9fb88598055896b57fca53936fc8dc2
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-03-16 07:42:49 +0900

    add gin

--- go.mod
+++ go.mod
@@ -5,5 +5,10 @@ go 1.15
 require (
 	cloud.google.com/go/firestore v1.5.0 // indirect
 	firebase.google.com/go v3.13.0+incompatible
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gin-gonic/gin v1.6.3
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
 	google.golang.org/api v0.41.0
 )
--- main.go
+++ main.go
@@ -1,24 +1,63 @@
 package main
 
 import (
-	"context"
-	"fmt"
+	"log"
+	"net/http"
 
-	firebase "firebase.google.com/go"
-
-	"google.golang.org/api/option"
+	"github.com/gin-gonic/gin"
 )
 
-func initAuth() (app *firebase.App, err error) {
-	opt := option.WithCredentialsFile("path/to/serviceAccountKey.json")
-	app, err = firebase.NewApp(context.Background(), nil, opt)
-	if err != nil {
-		return nil, fmt.Errorf("error initializing app: %v", err)
-	}
-	return app, nil
-}
-
 func main() {
-
-	fmt.Print("hello")
+	engine := gin.Default()
+	userEngine := engine.Group("/user")
+	{
+		v1 := userEngine.Group("/v1")
+		{
+			v1.GET("/", public)
+			// v1.GET("/public", private)
+		}
+	}
+	log.Fatal(engine.Run(":8080"))
 }
+
+func public(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"status": "ok",
+	})
+}
+
+// func private(c echo.Context) error {
+
+// 	ctx := c.Request().Context()
+// 	if ctx == nil {
+// 		ctx = context.Background()
+// 	}
+
+// 	opt := option.WithCredentialsFile("GOOGLE_APPLICATION_CREDENTIALS")
+// 	app, err := fire.NewApp(context.Background(), nil, opt)
+// 	if err != nil {
+// 		fmt.Printf("error: %v\n", err)
+// 		os.Exit(1)
+// 	}
+// 	auth, err := app.Auth(context.Background())
+// 	if err != nil {
+// 		fmt.Printf("error: %v\n", err)
+// 		os.Exit(1)
+// 	}
+
+// 	// クライアントから送られてきた JWT 取得
+// 	authHeader := c.Request().Header.Get("Authorization")
+// 	idToken := strings.Replace(authHeader, "Bearer ", "", 1)
+
+// 	// JWT の検証
+// 	token, err := auth.VerifyIDToken(context.Background(), idToken)
+// 	if err != nil {
+// 		u := fmt.Sprintf("error verifying ID token: %v\n", err)
+// 		return c.JSON(http.StatusBadRequest, u)
+// 	}
+// 	uid := token.Claims["user_id"]
+
+// 	u := fmt.Sprintf("public is success. uid is %s", uid)
+
+// 	return c.JSON(http.StatusOK, u)
+// }
