1a0ac6374436bac006a3a35df81f2ec0be40f7a8
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-03-22 07:58:25 +0900

    del files

--- domain/user.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-// User is domain
-type User struct {
-	UserName string `json:"user_name,omitempty"`
-	Email    string `json:"email,omitempty"`
-	Password string `json:"password,omitempty"`
-}
