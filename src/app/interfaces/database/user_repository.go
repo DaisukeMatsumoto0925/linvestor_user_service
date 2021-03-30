91b5071ec7622f5196e34f3a196a433dc3e38773
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-03-31 08:49:53 +0900

    fix fatail to return

--- src/app/interfaces/database/user_repository.go
+++ src/app/interfaces/database/user_repository.go
@@ -12,26 +12,26 @@ type UserRepository struct {
 	SQLHandler
 }
 
-// Store is func save a user
-func (repo *UserRepository) CreateUser(ctx context.Context, u domain.User) (domain.User, error) {
-	user, err := repo.PostUser(ctx, u)
+// CreateUser is func save a user
+func (repo *UserRepository) CreateUser(ctx context.Context, u domain.User) (user domain.User, err error) {
+	user, err = repo.PostUser(ctx, u)
 	if err != nil {
-		log.Fatalf("error creating user: %v\n", err)
+		return
 	}
 	log.Printf("Successfully created user: %#v\n", user)
 
-	return user, nil
+	return
 }
 
 // GetByID is func get a user
 func (repo *UserRepository) GetByID(ctx context.Context, uid string) (user domain.User, err error) {
 	user, err = repo.GetUser(ctx, uid)
 	if err != nil {
-		log.Fatalf("error getting user %s: %v\n", uid, err)
+		return
 	}
 	log.Printf("Successfully fetched user data: %v\n", user)
 
-	return user, nil
+	return
 }
 
 // DeleteByID is func delete a user
@@ -45,12 +45,12 @@ func (repo *UserRepository) DeleteByID(ctx context.Context, uid string) (err err
 }
 
 // UpdateUser is func update a user
-func (repo *UserRepository) UpdateUser(ctx context.Context, uid string, u domain.User) (domain.User, error) {
-	user, err := repo.PutUser(ctx, uid, u)
+func (repo *UserRepository) UpdateUser(ctx context.Context, uid string, u domain.User) (user domain.User, err error) {
+	user, err = repo.PutUser(ctx, uid, u)
 	if err != nil {
-		log.Fatalf("error creating user: %v\n", err)
+		return
 	}
 	log.Printf("Successfully created user: %#v\n", user)
 
-	return user, nil
+	return
 }
