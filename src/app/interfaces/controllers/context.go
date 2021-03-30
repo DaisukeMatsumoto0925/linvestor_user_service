3916c1aa0bfb0ef5ffc05eeeccb7522469d61f48
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-03-30 11:16:46 +0900

    add cmt

--- src/app/interfaces/controllers/context.go
+++ src/app/interfaces/controllers/context.go
@@ -2,6 +2,7 @@ package controllers
 
 import "time"
 
+// Context is context.Context
 type Context interface {
 	Param(string) string
 	Bind(interface{}) error
@@ -12,15 +13,3 @@ type Context interface {
 	Err() error
 	Value(key interface{}) interface{}
 }
-
-// {
-// 	"resource": "/Users/matsumotodaisuke/go/src/github.com/xfpng345/linvestor_user_service/src/app/infrastructure/sqlhandler.go",
-// 	"owner": "_generated_diagnostic_collection_name_#22",
-// 	"severity": 8,
-// 	"message": "cannot use ctx (variable of type interface{}) as context.Context value in argument to handler.Conn.CreateUser: missing method Deadline",
-// 	"source": "compiler",
-// 	"startLineNumber": 46,
-// 	"startColumn": 38,
-// 	"endLineNumber": 46,
-// 	"endColumn": 41
-// }
