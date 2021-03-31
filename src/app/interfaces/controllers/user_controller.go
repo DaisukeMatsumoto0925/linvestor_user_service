952db6fac74e6eced2d8bc3aadc2a3b862418392
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-03-31 09:30:00 +0900

    return json response

--- src/app/interfaces/controllers/user_controller.go
+++ src/app/interfaces/controllers/user_controller.go
@@ -8,6 +8,18 @@ import (
 	"github.com/xfpng345/linvestor_user_service/src/app/usecase"
 )
 
+type jsonResponse struct {
+	Status  int    `json:"status,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+func responseJSON(responseCode int, responseMsg string) jsonResponse {
+	return jsonResponse{
+		Status:  responseCode,
+		Message: responseMsg,
+	}
+}
+
 // UserController is struct of controller
 type UserController struct {
 	Interactor usecase.UserInteractor
@@ -29,13 +41,13 @@ func (controller *UserController) Create(c Context) {
 	var u domain.User
 	err := c.BindJSON(&u)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, "Bad request")
+		c.JSON(http.StatusBadRequest, responseJSON(http.StatusBadRequest, err.Error()))
 		return
 	}
 
 	user, err := controller.Interactor.UserCreate(c, u)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, err.Error())
+		c.JSON(http.StatusBadRequest, responseJSON(http.StatusBadRequest, err.Error()))
 	}
 	c.JSON(http.StatusOK, user)
 }
@@ -45,7 +57,7 @@ func (controller *UserController) Show(c Context) {
 	id := c.Param("id")
 	user, err := controller.Interactor.UserByID(c, id)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, err.Error())
+		c.JSON(http.StatusBadRequest, responseJSON(http.StatusBadRequest, err.Error()))
 	}
 	c.JSON(http.StatusOK, user)
 }
@@ -55,9 +67,9 @@ func (controller *UserController) Delete(c Context) {
 	id := c.Param("id")
 	err := controller.Interactor.DeleteByID(c, id)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, err.Error())
+		c.JSON(http.StatusBadRequest, responseJSON(http.StatusBadRequest, err.Error()))
 	}
-	c.JSON(http.StatusOK, "ok")
+	c.JSON(http.StatusOK, responseJSON(http.StatusOK, "Deleted a user"))
 }
 
 // Update func update a user
@@ -67,7 +79,7 @@ func (controller *UserController) Update(c Context) {
 	err := c.BindJSON(&u)
 	user, err := controller.Interactor.UpdateByID(c, id, u)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, err.Error())
+		c.JSON(http.StatusBadRequest, responseJSON(http.StatusBadRequest, err.Error()))
 	}
 	c.JSON(http.StatusOK, user)
 }
